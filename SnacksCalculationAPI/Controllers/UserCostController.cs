68fd8dc235518d97dff293fca6f1a8b61557221f
Azizul <islamazizul0000@gmail.com>
2024-06-02 22:18:39 +0600

    add update user cost

--- SnacksCalculationAPI/Controllers/UserCostController.cs
+++ SnacksCalculationAPI/Controllers/UserCostController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SnacksCalculationAPI.Models;
 using SnacksCalculationAPI.Filters;
+using Microsoft.EntityFrameworkCore;
 
 namespace SnacksCalculationAPI.Controllers
 {
@@ -40,6 +41,40 @@ namespace SnacksCalculationAPI.Controllers
                 response.IsError = true;
                 return response;
             }
+        }
+
+        [HttpPut("update")]
+        public async Task<ActionResult<ApiResponse>>Update(UserInformationModel model)
+        {
+            var response = new ApiResponse();
+            try
+            {
+                var dbModel = await _context.UserInformationModels.FirstOrDefaultAsync(x => x.UserId == model.UserId && x.Date==model.Date);
+
+                if (dbModel == null)
+                {
+                    response.Message = "Data not found";
+                    response.IsError = true;
+                    return response;
+                }
+                dbModel.Amount = model.Amount;
+  
+                dbModel.ItemName = model.ItemName;
+                _context.UserInformationModels.Update(dbModel);
+                await _context.SaveChangesAsync();
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.Message = "Data Updated";
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Result = null;
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                response.ResponseException = ex.Message;
+                response.IsError = true;
+                return response;
+            }
+
         }
 
 
