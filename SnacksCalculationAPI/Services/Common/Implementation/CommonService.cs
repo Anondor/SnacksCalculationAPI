e4647a471a49f2fc98e270f44f6bbed9ad6bd55f
Azizul <azizul@kaz-software.com>
2024-06-24 15:50:14 +0600

    done on excel sheet

--- SnacksCalculationAPI/Services/Common/Implementation/CommonService.cs
+++ SnacksCalculationAPI/Services/Common/Implementation/CommonService.cs
@@ -44,17 +44,35 @@ namespace SnacksCalculationAPI.Services.Common.Implementation
                                 Item = cm.Item,
                             };
             var result = await listQuery.ToListAsync();
-
+            var userQuery = _context.UserModels.AsQueryable();
+            /*
+            var sumByUserId = _context.UserInformationModels
+                    .GroupBy(t => t.UserId)
+                    .Select(g => new UserCostData
+                    {
+                        UserId = g.Key,
+                        Amount = g.Sum(t => t.Amount)
+                    })
+                    .ToList();
             
+            */
+   
 
-
+   
+          
+            var userList = await userQuery.ToListAsync();
             ExcelPackage excel = new ExcelPackage();
 
             var workSheet = excel.Workbook.Worksheets.Add("Sheet1");
             var headers = new List<string>();
-            var headersId= new List<int>();
-            headers =["Date","Super Admin","Admin","Normal User","Items"];
-            headersId = [1,2,3];    
+            var headersId = new List<int>();
+            headers = ["Date"];
+           for(int index=0;index<userList.Count;index++)
+            {
+                headers.Insert(index + 1, userList[index].Name);
+                headersId.Insert(index, userList[index].Id);
+            }
+            headers.Insert(userList.Count + 1, "Items"); 
 
             _file.SetTableStyle(workSheet, headers.Count);
             _file.SetHeaderStyle(workSheet, headers.Count);
@@ -70,9 +88,23 @@ namespace SnacksCalculationAPI.Services.Common.Implementation
 
         private void Insert_AuditReportExcelRows2(string toDate,List<int> headersId, List<UserCostModel> result, ExcelWorksheet workSheet)
         {
+            var perUserTotalAmount = _context.UserInformationModels
+                   .GroupBy(t => t.UserId)
+                   .Select(g => new UserCostData
+                   {
+                       UserId = g.Key,
+                       Amount = g.Sum(t => t.Amount)
+                   })
+                   .ToList();
+
             int startingRowNumberForData = 2;
             var date = toDate.Substring(0, 8);
             var rowNumber= Int32.Parse(toDate.Substring(8, 2));
+            List<double> perUserTotalCost = new List<double>();
+            for (int i = 0; i < headersId.Count; i++)
+            {
+                perUserTotalCost.Insert(i, 0);
+            }
 
             int column;
             for (int row=0;row< rowNumber; row++) {
@@ -89,6 +121,7 @@ namespace SnacksCalculationAPI.Services.Common.Implementation
                     if (index != -1)
                     {
                         workSheet.Cells[row + 2, column++].Value = result[index].Amount;
+                        perUserTotalCost[col] += result[index].Amount;
                         item = result[index].Item;
                     }
                     else
@@ -100,6 +133,21 @@ namespace SnacksCalculationAPI.Services.Common.Implementation
             }
              column = 1;
             workSheet.Cells[rowNumber + 2, column++].Value = "Remaining balance";
+            for (int col = 0; col < headersId.Count; col++)
+            {
+                double value = 0;
+                var index = perUserTotalAmount.FindIndex(x =>  x.UserId == headersId[col]);
+                if (index != -1)
+                {
+                    value = perUserTotalAmount[index].Amount;
+
+                }
+                workSheet.Cells[rowNumber+2, column++].Value = value- perUserTotalCost[col];
+
+            }
+            
+
+       
 
         }
     }
